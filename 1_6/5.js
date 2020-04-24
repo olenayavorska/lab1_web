3d4235f7fbe972adf42dbc28b5441a94b6d75af9
Olena <61748294+olenayavorska@users.noreply.github.com>
2020-04-24 09:16:47 +0300

    Update 5.js

--- 1_6/5.js
+++ 1_6/5.js
@@ -1,7 +1,7 @@
 //Написати функцію, яка поверне кількість днів, яка пройшла від початку року
 exports.date=()=>{ 
 let today = new Date();
-let startYear = new Date(2020, 1, 1, 00, 00, 00, 000); // Встановлюємодень та місяць
+let startYear = new Date('January 00, 2020 00:00:00 GMT+2:00'); // Встановлюємодень та місяць
 startYear.setFullYear(today.getFullYear()); // Встановлюємо цей рік
 let msPerDay = 24 * 60 * 60 * 1000; // Кількість мілісекунд в одному дні
 let daysLeft1 = (today.getTime()-startYear.getTime() ) / msPerDay;
